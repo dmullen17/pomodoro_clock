449cfc03c9357b544d18e6bd2f4fd3757fe1a66b
dmullen17 <dmullen17@gmail.com>
2019-06-07 10:03:02 -0700

    added countdown timer functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,50 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row'
 import './App.css';
+var accurateInterval = require('accurate-interval');
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isActive: false,
+            isActive: true,
             time: 1500, 
-            breakTime: 300
+            breakTime: 300,
+            interval: ''
         };
+        this.decrementTime = this.decrementTime.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.reset = this.reset.bind(this);
+        this.timeToMMSS = this.timeToMMSS.bind(this);
+        this.toggleActive = this.toggleActive.bind(this);
+    }
+    startTimer() {
+        this.setState({
+            interval: accurateInterval(this.decrementTime, 1000)
+        });
+    }
+    decrementTime() {
+        this.setState({
+            time: this.state.time - 1
+        });
+    }
+    reset() {
+        this.setState({
+            interval: ''
+        });
+    }
+    toggleActive() {
+        this.setState({
+            isActive: !this.state.isActive
+        });
+    }
+    timeToMMSS() {
+        let minutes = Math.floor(this.state.time / 60);
+        let seconds = this.state.time - minutes * 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        return minutes + ':' + seconds;
     }
     render() {
         return (
@@ -20,7 +55,7 @@ class App extends React.Component {
                 <Row className='row1'>
                     <Col className='clock'>
                         <Row className='timer'>
-                            25:00
+                            {this.timeToMMSS()}
                         </Row>
                     </Col>
                 </Row>
@@ -28,10 +63,12 @@ class App extends React.Component {
                     <ControllerColumn name='Interval' time='25:00'/>
                     <ControllerColumn name='Break Time' time='5:00'/>
                 </Row>
+            <div onClick={this.startTimer}>Start</div>
+            <div onClick={this.reset}>Reset</div>
             </Container>
         </div>
   );
-    }
+}
 }
 
 const ControllerColumn = (props) => {
