cde80d8c1a348fae4eee409b6cb8fb4eec0f3eb0
dmullen17 <dmullen17@gmail.com>
2019-06-07 15:57:26 -0700

    added styling and session/break functionalities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row'
 import './App.css';
+import "font-awesome/css/font-awesome.css"
 var accurateInterval = require('accurate-interval');
 
 
@@ -11,29 +12,47 @@ class App extends React.Component {
         super(props);
         this.state = {
             isActive: true,
-            time: 1500, 
-            breakTime: 300,
+            time: 1500,
+            session: 25, 
+            break: 5,
             interval: ''
         };
         this.decrementTime = this.decrementTime.bind(this);
+        this.displayTime = this.displayTime.bind(this);
         this.startTimer = this.startTimer.bind(this);
         this.reset = this.reset.bind(this);
-        this.timeToMMSS = this.timeToMMSS.bind(this);
         this.toggleActive = this.toggleActive.bind(this);
-    }
-    startTimer() {
-        this.setState({
-            interval: accurateInterval(this.decrementTime, 1000)
-        });
+        this.updateBreak = this.updateBreak.bind(this);
+        this.updateSession = this.updateSession.bind(this);
+        this.updateTimerControls = this.updateTimerControls.bind(this);
     }
     decrementTime() {
         this.setState({
             time: this.state.time - 1
         });
     }
+    displayTime() {
+        let minutes = Math.floor(this.state.time / 60);
+        let seconds = this.state.time - minutes * 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        return minutes + ':' + seconds;
+    }
+    updateTimerControls(value) {
+        
+    }
     reset() {
         this.setState({
+            isActive: true,
+            time: 1500,
+            session: 25, 
+            break: 5,
             interval: ''
+        });
+    }
+    startTimer() {
+        this.setState({
+            interval: accurateInterval(this.decrementTime, 1000)
         });
     }
     toggleActive() {
@@ -41,30 +60,38 @@ class App extends React.Component {
             isActive: !this.state.isActive
         });
     }
-    timeToMMSS() {
-        let minutes = Math.floor(this.state.time / 60);
-        let seconds = this.state.time - minutes * 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        return minutes + ':' + seconds;
+    updateSession(e) {
+        this.updateTimerControls('session', e.target.dataset.value);
     }
-    render() {
+    updateBreak(e) {
+        this.updateTimerControls('break', e.target.dataset.value);
+    }
+    updateTimerControls(timerCategory, value) {
+        if (this.state.isActive) return;
+        const num = value === '+' ? 1 : -1;
+        this.setState({
+            [timerCategory]: this.state[timerCategory] + num
+        });
+    }
+     render() {
         return (
         <div>
             <Container className="App" fluid={true}>
                 <Row className='row1'>
                     <Col className='clock'>
                         <Row className='timer' id='time-left'>
-                            {this.timeToMMSS()}
+                            {this.displayTime()}
                         </Row>
                     </Col>
                 </Row>
                 <Row className='row2'>
-                    <TimerControl name='Session Length' nameId='session-label' time='25:00'/>
-                    <TimerControl name='Break Length' nameId='break-label' time='5:00'/>
+                    <TimerControl name='Session Length' nameId='session-label' increment='session-increment' decrement='session-decrement' length='session-length' time={this.state.session} onClick={this.updateSession}/>
+                    <Col>
+                        <Row onClick={this.startTimer} className='circularButton' id='start_stop'>Start</Row>
+                        <Row onClick={this.reset} className='circularButton' id='reset'>Reset</Row>
+                    </Col>
+                    <TimerControl name='Break Length' nameId='break-label' increment='break-increment' decrement='break-decrement' length='break-length' time={this.state.break} onClick={this.updateBreak}/>
                 </Row>
-            <div onClick={this.startTimer}>Start</div>
-            <div onClick={this.reset}>Reset</div>
             </Container>
         </div>
   );
@@ -73,11 +100,11 @@ class App extends React.Component {
 
 const TimerControl = (props) => {
     return (
-        <Col className='TimerControl'>
+        <Col className='timerControl'>
             <Row id={props.nameId}>{props.name}</Row>
-            <Row>Up arrow</Row>
-            <Row>{props.time}</Row>
-            <Row>Down Arrow</Row>
+            <Row><i className="fa fa-arrow-up fa-2x" id={props.increment} data-value='+' onClick={props.onClick}/></Row>
+            <Row id={props.length}>{props.time}</Row>
+            <Row><i className="fa fa-arrow-down fa-2x" id={props.decrement} data-value='-' onClick={props.onClick}/></Row>
         </Col>
     )
 }
